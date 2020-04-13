ce1c0212c12bf1d45dee853f35ac634413ee8b77
Fedkin Sergey <iSimstix1@mail.ru>
2020-04-13 22:36:28 +0200

    Even_OR_Uneven_Numbers_Used_With_One_Loop

--- ConsoleApp_Skillbox/main.cpp
+++ ConsoleApp_Skillbox/main.cpp
@@ -1,34 +1,36 @@
 #include <iostream>
 #include <string>
 
-void Even_OR_Uneven_Numbers(const int N) {
-
+void Even_OR_Uneven_Numbers(const int N) 
+{
 	// Event numbers with use one loop;
 	for (int i = 0; i <= N; i++)
 	{
 		// Event numbers;
-		if (N % 2 == 0) {
-			{
+		if (N % 2 == 0) 
+		{
 				std::cout << i << " ";
 				i++;
-			}
 		}
 		// Unevent numbers;
-		else if (N % 1 == 0) {
-			i += 1;
-			std::cout << i << " ";
+		else if (N % 1 == 0) 
+		{
+				i += 1;
+				std::cout << i << " ";
 		}
 	}
 }
 
-int main() {
-
+int main() 
+{
 	// Decision 1;
 	std::cout << "Decision 1: " << std::endl;
 	const int Number = 20;
 	std::cout << "Even numbers: ";
-	for (int i = 0; i <= Number; i++) {
-		if (i % 2 == 0) {
+	for (int i = 0; i <= Number; i++) 
+	{
+		if (i % 2 == 0) 
+		{
 			std::cout << i++ << " ";
 		}
 	}
