d7e9f48b2c730b0c3ffc35f18df14058969aaefb
Fedkin Sergey <iSimstix1@mail.ru>
2020-04-13 18:33:29 +0200

    Even or uneven numbers

--- ConsoleApp_Skillbox/main.cpp
+++ ConsoleApp_Skillbox/main.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
-	
-	std::string platformName = "Skillbox";
+void Even_OR_Uneven_Numbers(const int N) {
 
-	std::cout << "Platfrom name    | " << platformName << std::endl;
-	std::cout << "Skillbox length  | " << platformName.length() << std::endl;
+	// Event numbers
+	if (N % 2 == 0) {
+		for (int i = 0; i <= N; i += 2) {
+			std::cout << i << " ";
+		}
+	}
+	// Uneven numbers
+	else if (N % 1 == 0) {
+		for (int i = 1; i <= N; i += 2) {
+			std::cout << i << " ";
+		}
+	}
+}
 
-	// Get the first character
-	std::cout << "The first symbol | " << platformName[platformName.length() - platformName.length()] << std::endl;
-	// Get last character
-	std::cout << "Last symbol      | " << platformName[platformName.length() - 1] << std::endl;
+int main() {
 
+	// TASK 1
+	const int Number = 20;
+	std::cout << "Even numbers: ";
+	for (int i = 0; i <= Number; i++) {
+		if (i % 2 == 0) {
+			std::cout << i++ << " ";
+		}
+	}
+
+	// TASK 2
+
+	// If const number is even
+	std::cout << "\nEven numbers (function: Even_OR_Uneven_Numbers): ";
+	Even_OR_Uneven_Numbers(20);
+	// If const number is uneven
+	std::cout << "\nUneven numbers (function: Even_OR_Uneven_Numbers): ";
+	Even_OR_Uneven_Numbers(21);
+
+	std::cout << std::endl;
+
+	return 0;
 }
