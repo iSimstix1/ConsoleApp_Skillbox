da85555c17a5d50912ca88329a1f3a69ec10f00d
Fedkin Sergey <iSimstix1@mail.ru>
2020-05-01 19:21:14 +0200

    Changed

--- ConsoleApp_Skillbox/main.cpp
+++ ConsoleApp_Skillbox/main.cpp
@@ -11,30 +11,32 @@ int main()
 
 	const int sizeArray = 3;
 
-	int array[sizeArray][sizeArray]= {
+	int array[sizeArray][sizeArray] {
 		{  1, 2, 3 }, 
 		{  4, 5, 6 },
 		{  7, 8, 9 }
 	};
 
+	int CheckingTheEquation = theCurrentNumberOfCalendar % sizeArray;
+	
 	for (int i = 0; i < sizeArray; i++) {
 		for (int j = 0; j < sizeArray; j++) {
 
 			array[i][j] = i + j;
 			std::cout << array[i][j] << " ";
 
-			int tmp = theCurrentNumberOfCalendar % sizeArray;
 			sumOfArraysInARow = array[i][j] + array[i][j];
 
-			if (array[i][j] == tmp) {
-				checkCondition = true;
+			if (array[i][j] == CheckingTheEquation) {
+				checkCondition == true;
 			}
 		}
+
 		if (checkCondition == true) {
 			std::cout << "| Amount on line: " << sumOfArraysInARow;
-			checkCondition = false;
 		}
 		std::cout << std::endl;
+		checkCondition = false;
 	}
 	return 0;
 }
