3422c9eb88c42900d908a14e98e3ce84bf76700e
Fedkin Sergey <iSimstix1@mail.ru>
2020-05-02 19:39:04 +0200

    Changed_MainClasses

--- ConsoleApp_Skillbox/main.cpp
+++ ConsoleApp_Skillbox/main.cpp
@@ -1,34 +1,69 @@
 #include <iostream>
 #include <string>
 #include <ctime>
+#include <cmath>
 
-int main()
-{
-	int theCurrentNumberOfCalendar = 4;
-	int sumOfArraysInARow = 0;
-	bool checkCondition = true;
+// Task 1
+class Car {
+private:
 
-	const int sizeArray = 4;
-	int array[sizeArray][sizeArray] { { } };
+	double maxSpeedCar;
+	double weightCar;
+	std::string colorCar;
 
-	int CheckingTheEquation = theCurrentNumberOfCalendar % sizeArray;
+public:
 
-	for (int i = 0; i < sizeArray; i++) {
-		for (int j = 0; j < sizeArray; j++) {
-
-			array[i][j] = i + j;
-			std::cout << array[i][j] << " ";
-
-			if (i == CheckingTheEquation) {
-				sumOfArraysInARow += array[i][j];
-				checkCondition = true;
-			}
-		}
-		if (checkCondition == true) {
-			std::cout << "| Amount on line: " << sumOfArraysInARow;
-			checkCondition = false;
-		}
-		std::cout << std::endl;
+	void SetCharacteristicCar(double setMaxSpeedCar, double setWeightCar, std::string setColorCar) {
+		maxSpeedCar = setMaxSpeedCar;
+		weightCar = setWeightCar;
+		colorCar = setColorCar;
 	}
+	void GetCharacteristicCar() {
+		std::cout << "Weight car: " << weightCar << std::endl;
+		std::cout << "Maximum speed car: " << maxSpeedCar << std::endl;
+		std::cout << "Color car: " << colorCar << std::endl;
+	}
+};
+
+// Task 2
+class Vector {
+private:
+
+	double x;
+	double y;
+	double z;
+
+	double LengthVector;
+
+public:
+	Vector() : x(3), y(4), z(0), LengthVector(0) {}
+	Vector(double _x, double _y, double _z, double _LengthVector) : x(_x), y(_y), z(_z), LengthVector(_LengthVector){}
+
+	void Show() {
+		std::cout << std::endl;
+		std::cout << "x = " << x << std::endl;
+		std::cout << "y = " << y << std::endl;
+		std::cout << "z = " << z << std::endl;
+	}
+
+	// Get vector by module
+	double Length() {
+		LengthVector = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+		return LengthVector;
+	}
+
+};
+
+int main() {
+	Car instanceCar;
+	instanceCar.SetCharacteristicCar(170, 1550, "White");
+	instanceCar.GetCharacteristicCar();
+
+	Vector instanceVector;
+	instanceVector.Show();
+	std::cout << "\nVector length (by module) = " << instanceVector.Length();
+	
+
+	std::cout << std::endl;
 	return 0;
 }
